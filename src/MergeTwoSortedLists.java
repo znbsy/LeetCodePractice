ac962f41dc08208556285ad3b10ba268621ee139
znbsy <yc138@rice.edu>
2022-12-08 20:34:43 -0500

    ...

---
+++ src/MergeTwoSortedLists.java
@@ -0,0 +1,35 @@
+public class MergeTwoSortedLists {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null){
+            return list2;
+        }
+        if(list2 == null){
+            return list1;
+        }
+        if(list1.val <= list2.val){
+            int value = list1.val;
+            ListNode next = this.mergeTwoLists(list1.next, list2);
+            return new ListNode(value, next);
+        }
+        else{
+            int value = list2.val;
+            ListNode next = this.mergeTwoLists(list1, list2.next);
+            return new ListNode(value, next);
+        }
+
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {
+
+    }
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(int val, ListNode next) {
+        this.val = val; this.next = next;
+    }
+}
