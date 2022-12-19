94eacf35fc59ceba1db8a5cef92430a1019e2eba
znbsy <yc138@rice.edu>
2022-12-19 17:24:45 -0500

    ...

---
+++ src/ReverseLinkedList.java
@@ -0,0 +1,13 @@
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while(current != null){
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+}
