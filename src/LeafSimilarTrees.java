e4188f683bb2f77f74a6b37a45dde3b26b0f769c
znbsy <yc138@rice.edu>
2023-03-13 00:00:23 -0400

    ...

--- src/LeafSimilarTrees.java
+++ src/LeafSimilarTrees.java
@@ -20,15 +20,3 @@ public class LeafSimilarTrees {
         return list;
     }
 }
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
--- src/MergeTwoSortedLists.java
+++ src/MergeTwoSortedLists.java
@@ -20,16 +20,3 @@ public class MergeTwoSortedLists {
 
     }
 }
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {
-
-    }
-    ListNode(int val) {
-        this.val = val;
-    }
-    ListNode(int val, ListNode next) {
-        this.val = val; this.next = next;
-    }
-}
