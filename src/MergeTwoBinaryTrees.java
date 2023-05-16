603c30534be4a6d63257f8c15bfd655df2e531af
znbsy <yc138@rice.edu>
2023-05-16 01:56:26 -0400

    ...

--- src/MergeTwoBinaryTrees.java
+++ src/MergeTwoBinaryTrees.java
@@ -7,10 +7,10 @@ public class MergeTwoBinaryTrees {
             return root1;
         }
 
-        TreeNode root = new TreeNode(root1.val + root2.val);
-        root.left = mergeTrees(root1.left, root2.left);
-        root.right = mergeTrees(root1.right, root2.right);
+        root1.val = root1.val + root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
 
-        return root;
+        return root1;
     }
 }
