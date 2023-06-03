42e788cb09b7286b6dfdc86afa71f4ef03f2429e
Yuqi Chen <yc138@rice.edu>
2023-06-03 05:27:18 -0500

    ...

--- src/MaxPointsOnALine.java
+++ src/MaxPointsOnALine.java
@@ -9,19 +9,19 @@ public class MaxPointsOnALine {
         for (int i = 0; i < points.length; i++) {
             for (int h = i + 1; h < points.length; h++) {
                 int count = 2;
-                double k;
-                double b;
+                int k;
+                int b;
                 int x1 = points[i][0];
                 int y1 = points[i][1];
                 int x2 = points[h][0];
                 int y2 = points[h][1];
                 if (x2 - x1 != 0) {
-                    k = (y2 -y1) / (double)(x2 - x1);
-                    b = (x2 * y1 - x1 * y2) / (double)(x2 - x1);
+                    k = y2 -y1;
+                    b = x2 * y1 - x1 * y2;
                     for (int j = h + 1; j < points.length; j++) {
                         int x3 = points[j][0];
                         int y3 = points[j][1];
-                        if (x3 * k + b == y3) {
+                        if (x3 * k + b == y3 * (x2 - x1)) {
                             count++;
                         }
                     }
