3b59482e29ca3b9b4dde51d1ef3da50e07b3a8dd
znbsy <yc138@rice.edu>
2023-06-13 02:58:37 -0400

    ...

--- src/GetMaximumInGeneratedArray.java
+++ src/GetMaximumInGeneratedArray.java
@@ -1,5 +1,39 @@
 public class GetMaximumInGeneratedArray {
-    public int getMaximumGenerated(int n) {
+    public static int getMaximumGenerated(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 0) {
+            return 0;
+        }
 
+        int[] mark = new int[n + 1];
+        mark[0] = 0;
+        mark[1] = 1;
+        int t = 2;
+        int max = 0;
+        while (t <= n) {
+            if (mark[t] != 0) {
+                t++;
+                continue;
+            }
+            int i = 1;
+            if (t % 2 == 0) {
+                mark[t] = mark[t / 2];
+            } else {
+                mark[t] = mark[t / 2] + mark[t / 2 + 1];
+            }
+            max = Math.max(mark[t], max);
+            while (i * t <= n) {
+                mark[i * t] = mark[t];
+                i *= 2;
+            }
+            t++;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMaximumGenerated(7));
     }
 }
