9e8c499b7c510fa370d14df279372f20f28f3e25
znbsy <yc138@rice.edu>
2023-06-21 06:40:30 -0400

    ...

---
+++ src/ReducingDishes.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class ReducingDishes {
+    public int maxSatisfaction(int[] satisfaction) {
+        int ans = 0;
+        int sum = 0;
+        Arrays.sort(satisfaction);
+        for (int i = satisfaction.length - 1; i >= 0; i--) {
+            if (sum + satisfaction[i] >= 0) {
+                ans = ans + sum + satisfaction[i];
+            } else {
+                continue;
+            }
+            sum += satisfaction[i];
+        }
+
+        return ans;
+    }
+}
