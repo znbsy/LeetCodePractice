2079a15a5f57b382e9e7c11830294508deebaecf
znbsy <yc138@rice.edu>
2023-07-19 11:21:27 -0400

    ...

---
+++ src/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class IntersectionOfTwoLinkedLists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+        return a;
+    }
+}
