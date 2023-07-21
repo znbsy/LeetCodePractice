9962d370b970b56bf07280f78a4bad18b587a4ab
znbsy <yc138@rice.edu>
2023-07-21 06:05:55 -0400

    ...

---
+++ src/InvertBinaryTree.java
@@ -0,0 +1,14 @@
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode node = root.left;
+        root.left = root.right;
+        root.right = node;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+}
