75f1ed414fee535f8d3df5368072893978cb7c44
znbsy <yc138@rice.edu>
2024-05-05 18:53:25 -0400

    ...

---
+++ src/IncreasingTripletSubsequence.java
@@ -0,0 +1,19 @@
+public class IncreasingTripletSubsequence {
+    public boolean increasingTriplet(int[] nums) {
+        int one = Integer.MAX_VALUE;
+        int two = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num <= one) {
+                one = num;
+            } else if (num <= two) {
+                two = num;
+            } else {
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+}
