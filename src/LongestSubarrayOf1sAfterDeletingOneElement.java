0e175ce415481ab911e89b940193225df3dcf782
znbsy <yc138@rice.edu>
2024-05-06 20:34:24 -0400

    ...

---
+++ src/LongestSubarrayOf1sAfterDeletingOneElement.java
@@ -0,0 +1,27 @@
+public class LongestSubarrayOf1sAfterDeletingOneElement {
+    public int longestSubarray(int[] nums) {
+        int left = 0;
+        int right = 0;
+        int max = 0;
+        int k = 1;
+
+        for (; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                k--;
+            }
+
+            if (k < 0) {
+                if (nums[left] == 0) {
+                    k++;
+                }
+                left++;
+            }
+
+            if (k >= 0) {
+                max = Math.max(max, right - left - 1);
+            }
+
+        }
+        return max;
+    }
+}
