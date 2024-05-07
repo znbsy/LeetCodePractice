b3df229d76abc346f451d25286f725232647d293
Yuqi Chen <yc138@rice.edu>
2024-05-07 08:56:50 -0500

    ...

--- src/DetermineIfTwoStringsAreClose.java
+++ src/DetermineIfTwoStringsAreClose.java
@@ -5,32 +5,29 @@ public class DetermineIfTwoStringsAreClose {
         char[] w1 = word1.toCharArray();
         char[] w2 = word2.toCharArray();
 
-        Arrays.sort(w1);
-        Arrays.sort(w2);
 
-        if (Arrays.equals(w1, w2)) {
-            return true;
-        }
+        char[] freq1 = new char[26];
+        char[] freq2 = new char[26];
 
-        Map<Character, Integer> m1 = new HashMap<>();
 
         for (var a: w1) {
-            m1.put(a, m1.getOrDefault(a, 0) + 1);
+            freq1[a - 'a'] += 1;
         }
-
-        Map<Character, Integer> m2 = new HashMap<>();
 
         for (var a: w2) {
-            m2.put(a, m2.getOrDefault(a, 0) + 1);
+            freq2[a - 'a'] += 1;
         }
 
-        List<Integer> m1ValuesSorted = new ArrayList<>(m1.values());
-        List<Integer> m2ValuesSorted = new ArrayList<>(m2.values());
-        Collections.sort(m1ValuesSorted);
-        Collections.sort(m2ValuesSorted);
+        for (int i = 0; i < 26; i++) {
+            if ((freq1[i] == 0 && freq2[i] != 0) || (freq1[i] != 0 && freq2[i] == 0)) {
+                return false;
+            }
+        }
 
-        return m1ValuesSorted.equals(m2ValuesSorted) &&
-                new HashSet<>(m1.keySet()).equals(new HashSet<>(m2.keySet()));
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
+
+        return Arrays.equals(freq1, freq2);
 
 
 
