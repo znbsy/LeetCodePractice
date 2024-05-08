e0e78619820bc19ad9501ea6252434370f97757b
Yuqi Chen <yc138@rice.edu>
2024-05-08 14:13:22 -0500

    ...

--- src/LeafSimilarTrees.java
+++ src/LeafSimilarTrees.java
@@ -1,22 +1,30 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LeafSimilarTrees {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        return this.findLeaf(root1).equals(this.findLeaf(root2));
+        List<Integer> seq1 = new ArrayList<>();
+        List<Integer> seq2 = new ArrayList<>();
+        findSeq(root1, seq1);
+        findSeq(root2, seq2);
+        return Arrays.equals(seq1.toArray(), seq2.toArray());
     }
-    private List<Integer> findLeaf(TreeNode root){
-        List<Integer> list = new ArrayList<>();
-        if(root.left == null && root.right == null){
-            list.add(root.val);
-            return list;
+
+    public void findSeq(TreeNode root, List<Integer> l) {
+
+        if (root.left == null && root.right == null) {
+            l.add(root.val);
+            return;
         }
-        if(root.left != null){
-            list.addAll(this.findLeaf(root.left));
+
+        if (root.left != null) {
+            findSeq(root.left, l);
         }
-        if(root.right != null){
-            list.addAll(this.findLeaf(root.right));
+
+        if (root.right != null) {
+            findSeq(root.right, l);
         }
-        return list;
+
     }
 }
