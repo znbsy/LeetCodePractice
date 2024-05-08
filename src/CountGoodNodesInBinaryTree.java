0ec1705fc0122079bf4e21bcbd703a5bb7d012df
Yuqi Chen <yc138@rice.edu>
2024-05-08 15:23:49 -0500

    ...

--- src/CountGoodNodesInBinaryTree.java
+++ src/CountGoodNodesInBinaryTree.java
@@ -1,4 +1,40 @@
 public class CountGoodNodesInBinaryTree {
+    int count = 0;
+
+    public int goodNodes(TreeNode root) {
+
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        if (root.left != null) {
+            helper(root.left, root.val);
+        }
+
+        if (root.right != null) {
+            helper(root.right, root.val);
+        }
+
+        return count + 1;
+
+    }
+
+    public void helper (TreeNode root, int max) {
+
+        if (root.val >= max) {
+            max = root.val;
+            count++;
+        }
+
+        if (root.left != null) {
+            helper(root.left, max);
+        }
+
+        if (root.right != null) {
+            helper(root.right, max);
+        }
 
 
+
+    }
 }
