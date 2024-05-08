41b932a4adaa4aca36ec8e2607ca80fd556056bf
Yuqi Chen <yc138@rice.edu>
2024-05-08 13:34:34 -0500

    ...

--- src/ReverseLinkedList.java
+++ src/ReverseLinkedList.java
@@ -1,13 +1,19 @@
 public class ReverseLinkedList {
     public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode current = head;
-        while(current != null){
-            ListNode next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
+        if (head == null || head.next == null) {
+            return head;
         }
-        return prev;
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = head.next;
+
+        while (next != null) {
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            next = next.next;
+        }
+        curr.next = prev;
+        return curr;
     }
 }
