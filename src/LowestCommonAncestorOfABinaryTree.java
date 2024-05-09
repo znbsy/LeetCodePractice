0c08ca3d29ef8fcebe2f01a58fd4d3306e3832f2
znbsy <yc138@rice.edu>
2024-05-09 14:35:22 -0400

    ...

--- src/LowestCommonAncestorOfABinaryTree.java
+++ src/LowestCommonAncestorOfABinaryTree.java
@@ -1,43 +1,29 @@
 public class LowestCommonAncestorOfABinaryTree {
 
-    TreeNode prev;
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 
 
-        check(root, p, q);
-
-        return prev;
-
-
-    }
-
-    public void check (TreeNode root, TreeNode p , TreeNode q) {
-
-        boolean left = helper(root.left, p) && helper(root.left, q);
-        boolean right = helper(root.right, p) && helper(root.right,q);
-
-
-
-        if (!left && !right) {
-            prev = root;
-        } else if (right) {
-            check(root.right, p, q);
-        } else {
-            check(root.left, p, q);
-        }
-
-
-    }
-
-    public boolean helper (TreeNode root, TreeNode p) {
         if (root == null) {
-            return false;
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
         }
 
-        if (root.left == p || root.right == p || root == p) {
-            return true;
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        } else if (left != null) {
+            return left;
+        } else {
+            return right;
         }
 
-        return helper(root.left, p) || helper(root.right, p);
+
     }
+
 }
