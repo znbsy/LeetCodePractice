b0e08a47031d0343d54c11e481e1f14d07280746
znbsy <yc138@rice.edu>
2024-05-09 16:46:12 -0400

    ...

--- src/MaximumLevelSumOfABinaryTree.java
+++ src/MaximumLevelSumOfABinaryTree.java
@@ -7,47 +7,41 @@ public class MaximumLevelSumOfABinaryTree {
         if (root == null) {
             return 0;
         }
-        Queue<TreeNode> queue = new LinkedList<>();
-        Map<Integer, Integer> level = new HashMap<>();
-        Map<TreeNode, Integer> map = new HashMap<>();
 
-
-
-        queue.add(root);
-        map.put(root, 1);
-        level.put(1, root.val);
-
-        while(!queue.isEmpty()) {
-            TreeNode curr = queue.poll();
-
-            if (curr.left != null) {
-                int next = map.get(curr) + 1;
-                queue.add(curr.left);
-                map.put(curr.left, next);
-                level.put(next, level.getOrDefault(next, 0) + curr.left.val);
-
-            }
-            if (curr.right != null) {
-                int next = map.get(curr) + 1;
-                queue.add(curr.right);
-                map.put(curr.right, next);
-                level.put(next, level.getOrDefault(next, 0) + curr.right.val);
-            }
-        }
+        List<Integer> compute = new ArrayList<>();
+        dfs(root, compute, 1);
+        System.out.println(compute.toString());
 
         int max = Integer.MIN_VALUE;
-        int ans = 0;
+        int lev = Integer.MAX_VALUE;
 
-        for (var e: level.entrySet()) {
-            int v = e.getValue();
-            int k = e.getKey();
-            if (v >= max && k < ans) {
+        for (int i = 0; i < compute.size(); i++) {
+            int v = compute.get(i);
+            if (v > max) {
+                lev = i;
                 max = v;
-                ans = k;
+            } else if (v == max) {
+                if (lev > i) {
+                    lev = i;
+                }
             }
         }
+        System.out.println(max);
+        return lev + 1;
 
-        return ans;
+    }
 
+    public void dfs(TreeNode root, List<Integer> list, int level) {
+        if (root == null) {
+            return;
+        }
+
+        if (level <= list.size()) {
+            list.set(level - 1, root.val + list.get(level - 1));
+        } else {
+            list.add(root.val);
+        }
+        dfs(root.left, list, level + 1);
+        dfs(root.right, list, level + 1);
     }
 }
