462a4f0018a38f17979802d131131957c39c8ef0
Yuqi Chen <yc138@rice.edu>
2024-05-13 11:33:07 -0500

    ...

--- src/SuccessfulPairsofSpellsAndPotions.java
+++ src/SuccessfulPairsofSpellsAndPotions.java
@@ -10,29 +10,30 @@ public class SuccessfulPairsofSpellsAndPotions {
         int[] pairs = new int[n];
 
         for (int i = 0; i < n; i++) {
-            if ((long) spells[i] * potions[0] >= success) {
-                pairs[i] = m;
-            }
-            if ((long) spells[i] * potions[m - 1] < success) {
-                pairs[i] = 0;
-            }
+//            if ((long) spells[i] * potions[0] >= success) {
+//                pairs[i] = m;
+//            }
+//            if ((long) spells[i] * potions[m - 1] < success) {
+//                pairs[i] = 0;
+//            }
             int left = 0;
             int right = m - 1;
             int mid = left + (right - left) / 2;
 
             while (left <= right) {
                 if ((long) spells[i] * potions[mid] >= success) {
-                    try {
-                        if ((long) spells[i] * potions[mid - 1] < success) {
-                            pairs[i] = m - mid;
-                            break;
-                        } else {
-                            right = mid - 1;
-                        }
-                    } catch (Exception e) {
-                        pairs[i] = m - mid;
-                        break;
-                    }
+//                    try {
+//                        if ((long) spells[i] * potions[mid - 1] < success) {
+//                            pairs[i] = m - mid;
+//                            break;
+//                        } else {
+//                            right = mid - 1;
+//                        }
+//                    } catch (Exception e) {
+//                        pairs[i] = m - mid;
+//                        break;
+//                    }
+                    right = mid - 1;
 
 
 
@@ -41,6 +42,7 @@ public class SuccessfulPairsofSpellsAndPotions {
                 }
                 mid = left + (right - left) / 2;
             }
+            pairs[i] = m - mid;
 
         }
 
