e8bd8ebbc584d01ce1bb4168f9c104fd0d36f6d3
znbsy <yc138@rice.edu>
2024-05-16 01:02:55 -0400

    ...

--- src/CountingBits.java
+++ src/CountingBits.java
@@ -1,20 +1,29 @@
 public class CountingBits {
     public int[] countBits(int n) {
+
         int[] ans = new int[n + 1];
 
-        for (int i = 0; i <= n; i++) {
-            int j = i;
-            while (j != 0) {
-                if (ans[j] != 0) {
-                    ans[i] += ans[j];
-                    break;
-                } else {
-                    ans[i] += j % 2;
-                    j /= 2;
-                }
-            }
+
+        for (int i = 1; i <= n; i++) {
+            ans[i] += ans[i / 2] + (i % 2);
         }
         return ans;
+
+//        int[] ans = new int[n + 1];
+//
+//        for (int i = 0; i <= n; i++) {
+//            int j = i;
+//            while (j != 0) {
+//                if (ans[j] != 0) {
+//                    ans[i] += ans[j];
+//                    break;
+//                } else {
+//                    ans[i] += j % 2;
+//                    j /= 2;
+//                }
+//            }
+//        }
+//        return ans;
     }
 
 }
