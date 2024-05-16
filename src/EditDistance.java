2f20e28273c2d3390350b8bbe48bf428d4ec09db
Yuqi Chen <yc138@rice.edu>
2024-05-15 23:12:42 -0500

    ...

--- src/EditDistance.java
+++ src/EditDistance.java
@@ -18,9 +18,27 @@ public class EditDistance {
 
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < m + 1; j++) {
+                int x = dp[i - 1][j - 1] + 1;
+                int y = dp[i][j - 1] + 1;
+                int z = dp[i - 1][j] + 1;
 
+                if (y <= z) {
+                    if (x <= y) {
+                        dp[i][j] = x;
+                    } else {
+                        dp[i][j] = y;
+                    }
+                } else {
+                    if (x <= z) {
+                        dp[i][j] = x;
+                    } else {
+                        dp[i][j] = z;
+                    }
+                }
             }
         }
+
+        return dp[n][m];
 
 
     }
