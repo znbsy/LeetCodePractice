dda432ab1033f8ff287b5ebb2ad4b874f4bebf41
Yuqi Chen <yc138@rice.edu>
2024-05-15 22:50:29 -0500

    ...

---
+++ src/EditDistance.java
@@ -0,0 +1,27 @@
+public class EditDistance {
+
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        dp[0][0] = 0;
+
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = 0;
+        }
+
+        for (int j = 0; j < m + 1; j++) {
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+
+            }
+        }
+
+
+    }
+}
