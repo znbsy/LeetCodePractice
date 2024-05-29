c68d5a67d3701cdab0750940b5d24aebdc0b3553
znbsy <yc138@rice.edu>
2024-05-29 17:35:32 -0400

    ...

--- src/RemoveDuplicatesFromSortedArrayII.java
+++ src/RemoveDuplicatesFromSortedArrayII.java
@@ -4,22 +4,22 @@ import java.util.Map;
 public class RemoveDuplicatesFromSortedArrayII {
     public int removeDuplicates(int[] nums) {
         int dup = 0;
-        Map<Integer, Integer> map = new HashMap<>();
+        int count = 1;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                int freq = map.get(nums[i]);
-                if (freq >= 2) {
-                    dup++;
-                } else {
-                    map.put(nums[i], freq + 1);
-                    nums[i - dup] = nums[i];
-                }
+        for (int i = 1; i < nums.length; i++) {
 
+            if (nums[i] == nums[i - 1]) {
+                count++;
             } else {
-                map.put(nums[i], 1);
-                nums[i - dup] = nums[i];
+                count = 1;
             }
+
+            if (count <= 2) {
+                nums[i - dup] = nums[i];
+            } else {
+                dup++;
+            }
+
         }
         return nums.length - dup;
     }
