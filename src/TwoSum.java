879b0f91f446673297a71d2714ba779f41205a66
znbsy <yc138@rice.edu>
2024-06-15 21:00:01 -0400

    ...

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -2,30 +2,14 @@ import java.util.*;
 
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        int[][] sortedNums = new int[nums.length][2];
-
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            sortedNums[i] = new int[]{i, nums[i]};
-        }
-
-
-
-        Arrays.sort(sortedNums, (a, b) -> Integer.compare(a[1], b[1]));
-        int left = 0;
-        int right = nums.length - 1;
-
-
-        while (left < right) {
-            int sum = sortedNums[left][1] + sortedNums[right][1];
-            if (sum == target) {
-                //System.out.println("hiiii");
-                return new int[]{sortedNums[left][0], sortedNums[right][0]};
-            } else if (sum < target) {
-                left++;
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
             } else {
-                right--;
+                map.put(nums[i], i);
             }
         }
-        return new int[]{sortedNums[left][0], sortedNums[right][0]};
+        return new int[]{};
     }
 }
