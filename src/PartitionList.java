7da7445d389a6ca27b93e5f6adfa2767d61d4daa
znbsy <yc138@rice.edu>
2024-06-22 16:34:04 -0400

    ...

---
+++ src/PartitionList.java
@@ -0,0 +1,25 @@
+public class PartitionList {
+    public ListNode partition(ListNode head, int x) {
+        ListNode less = new ListNode();
+        ListNode more = new ListNode();
+        ListNode l = less;
+        ListNode m = more;
+
+
+        while (head != null) {
+            if (head.val < x) {
+                l.next = head;
+                l = l.next;
+            } else {
+                m.next = head;
+                m = m.next;
+            }
+            head = head.next;
+        }
+
+        m.next = null;
+        l.next = more.next;
+
+        return less.next;
+    }
+}
