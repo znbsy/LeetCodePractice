c99ceff614b4795c40969b197e2a1f04822e2960
znbsy <yc138@rice.edu>
2024-06-24 17:18:45 -0400

    ...

--- src/NumberofIslands.java
+++ src/NumberofIslands.java
@@ -2,14 +2,15 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class NumberofIslands {
-    int ans = 0;
-    public int numIslands(char[][] grid) {
 
+    public int numIslands(char[][] grid) {
+        int ans = 0;
 
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == '1') {
                     helper(grid, i, j);
+                    ans++;
                 }
             }
         }
@@ -20,38 +21,22 @@ public class NumberofIslands {
 
     public void helper(char[][] grid, int i, int j) {
 
-        Queue<int[]> queue = new LinkedList<>();
-
-        queue.add(new int[]{i ,j});
         grid[i][j] = '0';
-
-
-        while (!queue.isEmpty()) {
-            int[] curr = queue.poll();
-
-            if (curr[0] + 1 < grid.length && grid[curr[0] + 1][curr[1]] == '1') {
-                queue.add(new int[]{curr[0] + 1,curr[1]});
-                grid[curr[0] + 1][curr[1]] = '0';
-            }
-
-            if (curr[0] - 1 >= 0 && grid[curr[0] - 1][curr[1]] == '1') {
-                queue.add(new int[]{curr[0] - 1,curr[1]});
-                grid[curr[0] - 1][curr[1]] = '0';
-            }
-
-            if (curr[1] + 1 < grid[0].length && grid[curr[0]][curr[1] + 1] == '1') {
-                queue.add(new int[]{curr[0],curr[1] + 1});
-                grid[curr[0]][curr[1] + 1] = '0';
-            }
-
-            if (curr[1] - 1 >= 0 && grid[curr[0]][curr[1] - 1] == '1') {
-                queue.add(new int[]{curr[0],curr[1] - 1});
-                grid[curr[0]][curr[1] - 1] = '0';
-            }
-
-
+        if (i - 1 >= 0 && grid[i - 1][j] == '1') {
+            helper(grid, i - 1, j);
         }
-        ans++;
+
+        if (i + 1 < grid.length && grid[i + 1][j] == '1') {
+            helper(grid, i + 1, j);
+        }
+
+        if (j - 1 >= 0 && grid[i][j - 1] == '1') {
+            helper(grid, i, j - 1);
+        }
+
+        if (j + 1 < grid[0].length && grid[i][j + 1] == '1') {
+            helper(grid, i, j + 1);
+        }
 
 
     }
