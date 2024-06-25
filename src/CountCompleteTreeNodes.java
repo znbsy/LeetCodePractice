f4fd287990aa436de82657a6f1ced0bb266163b0
znbsy <yc138@rice.edu>
2024-06-25 16:48:35 -0400

    ...

--- src/CountCompleteTreeNodes.java
+++ src/CountCompleteTreeNodes.java
@@ -3,23 +3,7 @@ public class CountCompleteTreeNodes {
     public int countNodes(TreeNode root) {
         if (root == null) return 0;
 
-        int count = 0;
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-
-        while (!queue.isEmpty()) {
-            TreeNode curr = queue.poll();
-            count++;
-            if (curr.left != null) {
-                queue.add(curr.left);
-            }
-            if (curr.right != null) {
-                queue.add(curr.right);
-            }
-        }
-
-        return count;
+        return countNodes(root.left) + countNodes(root.right) + 1;
 
 
     }
