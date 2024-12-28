4c8cea51081f1badba6c4f06a7b12cbae0492328
znbsy <yc138@rice.edu>
2024-12-28 01:48:49 -0500

    ...

--- src/MergekSortedLists.java
+++ src/MergekSortedLists.java
@@ -2,21 +2,38 @@ import java.util.*;
 
 public class MergekSortedLists {
     public ListNode mergeKLists(ListNode[] lists) {
-        List<Integer> temp = new ArrayList<>();
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
 
         for (var l: lists) {
             while (l != null) {
-                temp.add(l.val);
+                min = Math.min(min, l.val);
+                max = Math.max(max, l.val);
                 l = l.next;
             }
         }
 
-        Collections.sort(temp);
+        ListNode[] arr = new ListNode[max - min + 1];
+        for (int i = lists.length - 1; i >= 0; i--){
+            ListNode curr = lists[i];
+            while (curr != null) {
+                ListNode temp = curr.next;
+                curr.next = arr[curr.val - min];
+                arr[curr.val - min] = curr;
+                curr = temp;
+
+            }
+
+        }
+
         ListNode dummy = new ListNode(-1);
         ListNode curr = dummy;
-        for (var a: temp){
-            curr.next = new ListNode(a);
-            curr = curr.next;
+        for (var a: arr) {
+            while (a != null) {
+                curr.next = new ListNode(a.val);
+                curr = curr.next;
+                a = a.next;
+            }
         }
         return dummy.next;
     }
