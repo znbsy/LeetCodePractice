b0cf26c3c0f04a7ca010abe6d618335e16c5a0ac
znbsy <yc138@rice.edu>
2026-02-25 20:35:30 -0600

    ...

---
+++ src/FindTheTownJudge.java
@@ -0,0 +1,18 @@
+public class FindTheTownJudge {
+    public int findJudge(int n, int[][] trust) {
+        int[] indegree = new int[n];
+        int[] outdegree = new int[n];
+
+        for (int[] ints : trust) {
+            indegree[ints[1] - 1]++;
+            outdegree[ints[0] - 1]++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == n - 1 && outdegree[i] == 0) {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+}
